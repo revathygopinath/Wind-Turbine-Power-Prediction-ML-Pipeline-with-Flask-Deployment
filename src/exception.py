4c5c02811f9a7755fe3f3cfb9b98b3da1daacd8d
Revathy Gopinath <revathygopinathm@gmail.com>
2025-04-26 15:24:26 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,10 @@
+#import sys
+#from src.logger import logging
+
+
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
